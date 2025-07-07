3b4efdce7e7475ed481df1afe238e741c500bfb8
Serhii Turlak <turlakserhii@gmail.com>
2025-07-07 18:15:21 +0300

    Add CORS

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -7,10 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyMethod()
+              .AllowAnyHeader()
+              .WithOrigins("http://localhost:4200", "https://localhost:4200");
+    });
+});
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
+app.UseCors("CorsPolicy");
 app.MapControllers();
-
 app.Run();
