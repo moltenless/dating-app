4724161630a70e17b11f6336c1953528c1d2c14f
Serhii Turlak <turlakserhii@gmail.com>
2025-07-10 15:54:24 +0300

    Login and logout account via Angular

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../core/services/account-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
+  private accountService = inject(AccountService)
+  protected creds: any = {}
+  protected loggedIn = signal(false);
 
+  login() {
+    this.accountService.login(this.creds).subscribe({
+      next: result => {
+        console.log(result)
+        this.loggedIn.set(true);
+        this.creds = {};
+      },
+      error: error => alert(error.message)
+    })
+  }
+
+  logout() {
+    this.loggedIn.set(false);
+  }
 }
+
