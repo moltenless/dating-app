6a9ec599f24628b65983ce19eadd707e84599a0d
Serhii Turlak <turlakserhii@gmail.com>
2025-07-10 17:59:59 +0300

    Display register component

--- client/src/features/home/home.ts
+++ client/src/features/home/home.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Register } from "../account/register/register";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Register],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  protected registerMode = signal(false);
 
+  showRegister() {
+    this.registerMode.set(true);
+  }
 }
