3303c15e458764aab40170746f7bbf1a2ca0fdfa
Serhii Turlak <turlakserhii@gmail.com>
2025-07-11 08:58:50 +0300

    Implement user registration and components communication

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RegisterCreds } from '../../../types/User';
+import { RegisterCreds, User } from '../../../types/User';
+import { AccountService } from '../../../core/services/account-service';
 
 @Component({
   selector: 'app-register',
@@ -9,13 +10,21 @@ import { RegisterCreds } from '../../../types/User';
   styleUrl: './register.css'
 })
 export class Register {
+  private accountService = inject(AccountService);
+  cancelRegister = output<boolean>();
   protected creds = {} as RegisterCreds;
 
   register() {
-    console.log(this.creds);
+    this.accountService.register(this.creds).subscribe({
+      next: response => {
+        console.log(response);
+        this.cancel();
+      },
+      error: error => console.error(error)
+    })
   }
 
   cancel() {
-    console.log('cancelled!');
+    this.cancelRegister.emit(false);
   }
 }
