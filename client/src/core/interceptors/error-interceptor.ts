32fd79fe036df7da85044fd128fd5a15a5d489be
Serhii Turlak <turlakserhii@gmail.com>
2025-07-11 19:03:56 +0300

    Add angular interceptors

---
+++ client/src/core/interceptors/error-interceptor.ts
@@ -0,0 +1,35 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { ToastService } from '../services/toast-service';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const toastService = inject(ToastService)
+  const router = inject(Router);
+
+  return next(req).pipe(
+    catchError(error => {
+      if (error) {
+        switch (error.status) {
+          case 400:
+            toastService.error(error.error);
+            break;
+          case 401:
+            toastService.error('Unauthorized');
+            break;
+          case 404:
+            toastService.error('Not found');
+            break;
+          case 500:
+            toastService.error('Server error');
+            break;
+          default:
+            toastService.error('Something unexpected went wrong'); 
+            break;
+        }
+      }
+      throw error;
+    })
+  );
+};
