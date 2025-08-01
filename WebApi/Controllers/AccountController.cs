7ea5a93185318d797414c527aa70aaa597a0b186
Serhii Turlak <turlakserhii@gmail.com>
2025-08-01 09:27:58 +0300

    Hook up the form to the api

--- WebApi/Controllers/AccountController.cs
+++ WebApi/Controllers/AccountController.cs
@@ -29,7 +29,15 @@ public class AccountController(
             DisplayName = registerDto.DisplayName,
             Email = registerDto.Email,
             PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-            PasswordSalt = hmac.Key
+            PasswordSalt = hmac.Key,
+            Member = new Member
+            {
+                DisplayName = registerDto.DisplayName,
+                Gender = registerDto.Gender,
+                City = registerDto.City,
+                Country = registerDto.Country,
+                DateOfBirth = registerDto.DateOfBirth,
+            }
         };
 
         context.Users.Add(user);
--- WebApi/DTO/RegisterDto.cs
+++ WebApi/DTO/RegisterDto.cs
@@ -7,7 +7,7 @@ public class RegisterDto
 {
     [Required]
     public string DisplayName { get; set; } = null!;
-    
+
     [Required]
     [EmailAddress]
     public string Email { get; set; } = null!;
@@ -15,4 +15,16 @@ public class RegisterDto
     [Required]
     [MinLength(4)]
     public string Password { get; set; } = null!;
+
+    [Required]
+    public string Gender { get; set; } = string.Empty;
+
+    [Required]
+    public string City { get; set; } = string.Empty;
+
+    [Required]
+    public string Country { get; set; } = string.Empty;
+    
+    [Required] 
+    public DateOnly DateOfBirth { get; set; }
 }
