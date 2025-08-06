c5f4a6b02f4d32c641271b42d3118fafedbe2835
Serhii Turlak <turlakserhii@gmail.com>
2025-08-06 12:53:24 +0300

    Improve the messages table

--- client/src/features/messages/messages.ts
+++ client/src/features/messages/messages.ts
@@ -3,16 +3,19 @@ import { MessageService } from '../../core/services/message-service';
 import { PaginatedResult } from '../../types/pagination';
 import { Message } from '../../types/message';
 import { Paginator } from "../../shared/paginator/paginator";
+import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages',
-  imports: [Paginator],
+  imports: [Paginator, RouterLink, DatePipe],
   templateUrl: './messages.html',
   styleUrl: './messages.css'
 })
 export class Messages implements OnInit {
   private messageService = inject(MessageService);
   protected container = 'Inbox';
+  protected fetchedContainer = 'Inbox';
   protected pageNumber = 1;
   protected pageSize = 10;
   protected paginatedMessages = signal<PaginatedResult<Message> | null>(null);
@@ -29,12 +32,15 @@ export class Messages implements OnInit {
   loadMessages() {
     this.messageService.getMessages(this.container, this.pageNumber, this.pageSize).
       subscribe({
-        next: response => this.paginatedMessages.set(response)
+        next: response => {
+          this.paginatedMessages.set(response);
+          this.fetchedContainer = this.container;
+        }
       });
   }
 
   get isInbox() {
-    return this.container === 'Inbox';
+    return this.fetchedContainer === 'Inbox';
   }
 
   setContainer(container: string){
