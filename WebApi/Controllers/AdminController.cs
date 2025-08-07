539d88243c89e19f6d66b661a20b7877d02bc3a1
Serhii Turlak <turlakserhii@gmail.com>
2025-08-07 10:43:58 +0300

    Edit the roles using admin role

--- WebApi/Controllers/AdminController.cs
+++ WebApi/Controllers/AdminController.cs
@@ -29,6 +29,28 @@ public class AdminController(UserManager<AppUser> userManager) : BaseApiControll
 
         return Ok(userList);
     }
+
+    [Authorize(Policy = "RequireAdminRole")]
+    [HttpPost("edit-roles/{userId}")]
+    public async Task<ActionResult<IList<string>>> EditRoles(string userId, [FromQuery] string roles)
+    {
+        if (string.IsNullOrEmpty(roles)) return BadRequest("You must seelect at least one role");
+
+        var selectedRoles = roles.Split(",").ToArray();
+
+        var user = await userManager.FindByIdAsync(userId);
+        if (user == null) return BadRequest("Could not retrieve user");
+
+        var userRoles = await userManager.GetRolesAsync(user);
+        var result = await userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+        if (!result.Succeeded) return BadRequest("Failed to add to roles");
+        result = await userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+        if (!result.Succeeded) return BadRequest("Added new roles but failed to remove from roles");
+
+        return Ok(await userManager.GetRolesAsync(user));
+    }
     
     [Authorize(Policy = "ModeratePhotoRole")]
     [HttpGet("photos-to-moderate")]
