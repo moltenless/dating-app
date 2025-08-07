53556bbe0132d713d6d9e03219fcbfe38d1e6aeb
Serhii Turlak <turlakserhii@gmail.com>
2025-08-07 11:01:08 +0300

    Add an admin guard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -14,6 +14,7 @@ import { MemberPhotos } from '../features/members/member-photos/member-photos';
 import { memberResolver } from '../features/members/member-resolver';
 import { preventUnsavedChangesGuard } from '../core/guards/prevent-unsaved-changes-guard';
 import { Admin } from '../features/admin/admin';
+import { adminGuard } from '../core/guards/admin-guard';
 
 export const routes: Routes = [
     { path: '', component: Home },
@@ -37,7 +38,7 @@ export const routes: Routes = [
             },
             { path: 'lists', component: Lists },
             { path: 'messages', component: Messages },
-            { path: 'admin', component: Admin },
+            { path: 'admin', component: Admin, canActivate: [adminGuard] },
         ]
     },
     { path: 'errors', component: TestErrors },
