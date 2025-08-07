e6baf9935fa3e255847753efc706cc9bac4ae023
Serhii Turlak <turlakserhii@gmail.com>
2025-08-07 12:05:55 +0300

    Add the edit roles modal

--- client/src/core/services/admin-service.ts
+++ client/src/core/services/admin-service.ts
@@ -14,4 +14,8 @@ export class AdminService {
     return this.http.get<User[]>(this.baseUrl + 'admin/users-with-roles');
   }
   
+  updateUserRoles(userId: string, roles: string[]) {
+    return this.http.post<string[]>(this.baseUrl + 'admin/edit-roles/'
+       + userId + '?roles=' + roles, {});
+  }
 }
