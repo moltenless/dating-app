37e18a2313700023c7e4edca026f2bcf3e62cdcf
Serhii Turlak <turlakserhii@gmail.com>
2025-08-08 16:42:59 +0300

    Toggle like and go back on member details

--- client/src/features/members/member-detailed/member-detailed.ts
+++ client/src/features/members/member-detailed/member-detailed.ts
@@ -6,6 +6,7 @@ import { AgePipe } from '../../../core/pipes/age-pipe';
 import { AccountService } from '../../../core/services/account-service';
 import { MemberService } from '../../../core/services/member-service';
 import { PresenceService } from '../../../core/services/presence-service';
+import { LikesService } from '../../../core/services/likes-service';
 
 @Component({
   selector: 'app-member-detailed',
@@ -18,11 +19,20 @@ export class MemberDetailed implements OnInit{
   protected memberService = inject(MemberService);
   private router = inject(Router);
   private accountService = inject(AccountService);
+  protected likesService = inject(LikesService);
   protected presenceService = inject(PresenceService);
   protected title = signal<string | undefined>('Profile');
+  private routeId = signal<string | null>(null);
   protected isCurrentUser = computed(() => {
-    return this.accountService.currentUser()?.id === this.route.snapshot.paramMap.get('id')
+    return this.accountService.currentUser()?.id === this.routeId()
   })
+  protected hasLiked = computed(() => this.likesService.likeIds().includes(this.routeId()!));
+
+  constructor() {
+    this.route.paramMap.subscribe(params => {
+        this.routeId.set(params.get('id'))
+    })
+  }
 
   ngOnInit(): void {
     this.title.set(this.route.firstChild?.snapshot?.title)
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -35,6 +35,11 @@ export class Nav implements OnInit{
     if (elem) elem.blur();
   }
 
+  handleSelectUserItem() {
+    const elem = document.activeElement as HTMLDivElement;
+    if (elem) elem.blur();
+  }
+
   login() {
     this.loading.set(true);
     this.accountService.login(this.creds).subscribe({
