356f4620fc35af0d56bafe474d168a29d3e3b323
Serhii Turlak <turlakserhii@gmail.com>
2025-08-08 11:33:25 +0300

    Send message via the hub

--- WebApi/SignalR/MessageHub.cs
+++ WebApi/SignalR/MessageHub.cs
@@ -2,13 +2,17 @@ using System;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Primitives;
+using WebApi.DTO;
+using WebApi.Entities;
 using WebApi.Extensions;
 using WebApi.Interfaces;
 
 namespace WebApi.SignalR;
 
 [Authorize]
-public class MessageHub(IMessageRepository messageRepository) : Hub
+public class MessageHub(
+    IMessageRepository messageRepository,
+    IMemberRepository memberRepository) : Hub
 {
     public override async Task OnConnectedAsync()
     {
@@ -22,6 +26,30 @@ public class MessageHub(IMessageRepository messageRepository) : Hub
         var messages = await messageRepository.GetMessageThread(GetUserId(), otherUser);
 
         await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
+    }
+
+    public async Task SendMessage(CreateMessageDto dto)
+    {
+        var sender = await memberRepository.GetMemberByIdAsync(GetUserId());
+        var recipient = await memberRepository.GetMemberByIdAsync(dto.RecipientId);
+
+        if (sender == null || recipient == null || sender.Id == dto.RecipientId)
+            throw new HubException("Cannot send the message");
+
+        var message = new Message
+        {
+            SenderId = sender.Id,
+            RecipientId = recipient.Id,
+            Content = dto.Content
+        };
+
+        messageRepository.AddMessage(message);
+
+        if (await messageRepository.SaveAllChangesAsync())
+        {
+            var group = GetGroupName(sender.Id, recipient.Id);
+            await Clients.Group(group).SendAsync("NewMessage", message.ToDto());    
+        }
     }
 
     public override Task OnDisconnectedAsync(Exception? exception)
